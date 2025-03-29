d8072c660d6f51619dd5fc66a39ee1d88f78b029
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-29 16:41:28 +0900

    feat :: range step 추가

--- src/functions/range.ts
+++ src/functions/range.ts
@@ -1,7 +1,12 @@
-export default function range(start: number, end: number = start)  {
+export default function range(start: number, end: number = start, step: number = 1)  {
   const returnValue:number[] = [];
-  for(let i = start; i < end; i++) {
+  for(let i = start; i < end*step; i+=step) {
     returnValue.push(i);
+  }
+  if(step == 0){
+    for(let i = start; i < end; i++) {
+      returnValue.push(0);
+    }
   }
   return returnValue;
 }
