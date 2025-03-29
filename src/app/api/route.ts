3f15af1dcff2b09f279ef6ddbbb78a7e3f90836e
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-29 16:43:36 +0900

    feat :: 데이터 추가

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,12 @@
 import {NextResponse} from "next/server";
 import type {TableProps} from "@/Type";
+import range from '@/functions/range'
 
 export async function GET(){
   const data:TableProps = {
     column : 5,
     row : 5,
-    data : [[]]
+    data : [range(0,5),range(5,10),range(10,15),range(15,20),range(20,25)]
   }
   return NextResponse.json(data);
 }
